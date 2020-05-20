bc2386670589bf82112f99ac295e1c7e684610c7
Irfan Ezani <irfanezani@gmail.com>
2020-05-21 04:44:12 +0800

    js function for country.js

--- js/app.js
+++ js/app.js
@@ -1,11 +1,20 @@
-/* ---------------------------------------------- */
-/*            CODE EXPLAINED TUTORIALS            */
-/*         www.youtube.com/CodeExplained          */
-/* ---------------------------------------------- */
 
-/* ---------------------------------------------- */
-/*                API URL AND KEY                 */
-/* ---------------------------------------------- */
+const country_name_element = document.querySelector(".country .name")
+const total_cases_element = document.querySelector(".total-cases .value")
+const new_cases_element = document.querySelector(".total-cases .new-value")
+
+const recover_element = document.querySelector(".recovered .name")
+const new_recover_element = document.querySelector(".recovered .new-value")
+
+const deaths_element = document.querySelector(".deaths .name")
+const new_deaths_element = document.querySelector(".deaths .new-value")
+
+const ctx = document.getElementById("axes_line_chart").getContext("2d")
+
+//variables
+let app_data = [];
+cases
+
 /*
 fetch(`https://covid19-monitor-pro.p.rapidapi.com/coronavirus/cases_by_days_by_country.php?country=country`, {
 		"method": "GET",
